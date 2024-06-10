429dc18be8ec639cf517eeb1022218c6a347a8d9
simone <simone.recupero@intre.it>
2024-06-10 11:08:41 +0200

    Feature n. 1

--- src/main/java/intre/it/javaoperator/controller/WebSiteReconciler.java
+++ src/main/java/intre/it/javaoperator/controller/WebSiteReconciler.java
@@ -1,9 +1,11 @@
 package intre.it.javaoperator.controller;
 
 import intre.it.javaoperator.model.WebSite;
+import io.fabric8.kubernetes.api.model.*;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.javaoperatorsdk.operator.api.reconciler.*;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
 
 @ControllerConfiguration
 public class WebSiteReconciler implements Reconciler<WebSite>, Cleaner<WebSite> {
@@ -15,6 +17,22 @@ public class WebSiteReconciler implements Reconciler<WebSite>, Cleaner<WebSite> 
 
     @Override
     public UpdateControl<WebSite> reconcile(WebSite WebSite, Context<WebSite> context) throws Exception {
+        Pod pod = new PodBuilder()
+                .withKind("Pod")
+                .withApiVersion("v1")
+                .withMetadata(
+                        new ObjectMetaBuilder()
+                        .withName(WebSite.getSpec().getWebSiteName()) // Leggo le informazioni della Custom Resource Creata
+                        .build())
+                .withSpec(new PodSpecBuilder()
+                        .withContainers(new ContainerBuilder()
+                                .withName(WebSite.getSpec().getWebSiteName()) // OPZIONALE: Leggo le informazioni della Custom Resource Creata
+                                .withImage("nginx:1.14.2")
+                                .build())
+                        .build())
+                .build();
+
+        client.pods().resource(pod).create();
         return UpdateControl.patchStatus(WebSite);
     }
 
