3e915101b46b1124960168eca41ad65da1cc0856
simone <simone.recupero@intre.it>
2024-06-11 09:11:23 +0200

    Revert "Exercise Nr. 4"
    This reverts commit 40a048e3983ab6126c0a6443f064717ece424cd1.

--- src/main/java/intre/it/javaoperator/controller/WebSiteReconciler.java
+++ src/main/java/intre/it/javaoperator/controller/WebSiteReconciler.java
@@ -1,16 +1,11 @@
 package intre.it.javaoperator.controller;
 
 import intre.it.javaoperator.model.WebSite;
-import io.fabric8.kubernetes.api.model.*;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.javaoperatorsdk.operator.api.reconciler.*;
-import io.javaoperatorsdk.operator.api.reconciler.Context;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerConfiguration
 public class WebSiteReconciler implements Reconciler<WebSite>, Cleaner<WebSite> {
@@ -22,18 +17,6 @@ public class WebSiteReconciler implements Reconciler<WebSite>, Cleaner<WebSite> 
 
     @Override
     public UpdateControl<WebSite> reconcile(WebSite WebSite, Context<WebSite> context) throws Exception {
-        Map<String, String> html = new HashMap<>();
-        html.put("index.html", "<html><body>" + WebSite.getSpec().getShortDescription() + "</body></html>");
-
-        ConfigMap configMap = new ConfigMapBuilder()
-                .withNewMetadata()
-                .withName(WebSite.getSpec().getWebSiteName()+"-cm")
-                .addToLabels("app", WebSite.getSpec().getWebSiteName())
-                .endMetadata()
-                .withData(html)
-                .build();
-        client.configMaps().resource(configMap).create();
-
         Deployment deployment = new DeploymentBuilder()
                     .withNewMetadata()
                         .withName(WebSite.getSpec().getWebSiteName()+"-deployment")
@@ -55,22 +38,7 @@ public class WebSiteReconciler implements Reconciler<WebSite>, Cleaner<WebSite> 
                                 .addNewPort()
                                     .withContainerPort(80)
                                 .endPort()
-                                .addNewVolumeMount()
-                                    .withName("nginx-conf")
-                                    .withSubPath("index.html")
-                                    .withMountPath("/usr/share/nginx/html/index.html")
-                                .endVolumeMount()
                             .endContainer()
-                            .addNewVolume()
-                                .withName("nginx-conf")
-                                .withNewConfigMap()
-                                .withName(WebSite.getSpec().getWebSiteName()+"-cm")
-                                    .addNewItem()
-                                        .withKey("index.html")
-                                        .withPath("index.html")
-                                    .endItem()
-                                .endConfigMap()
-                            .endVolume()
                         .endSpec()
                     .endTemplate()
                     .endSpec()
@@ -84,7 +52,6 @@ public class WebSiteReconciler implements Reconciler<WebSite>, Cleaner<WebSite> 
     @Override
     public DeleteControl cleanup(WebSite webSite, Context<WebSite> context) {
         client.apps().deployments().withName(webSite.getSpec().getWebSiteName()+"-deployment").delete(); // cancello il deployment
-        client.configMaps().withName(webSite.getSpec().getWebSiteName()+"-cm").delete(); // cancello la configmap
         return DeleteControl.defaultDelete();
     }
 }
